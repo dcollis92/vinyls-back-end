d2598b6ddbe3ce4e2ef14ca4fb7673ebc608325e
provideforme <evan.a.bernard@gmail.com>
2022-03-26 16:54:56 -0400

    change api call function

--- controllers/records.js
+++ controllers/records.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 function getRecord(req, res) {
-  console.log('hello');
-  axios.get(`https://api.discogs.com/database/search?q=${req.body.search}=${process.env.CONSUMER_KEY}&secret=${process.env.CONSUMER_SECRET}`)
+    axios.get(`https://api.discogs.com/database/search?q=${req.body.query}&{?type,title,release_title,artist,}&key=${process.env.CONSUMER_KEY}&secret=${process.env.CONSUMER_SECRET}`)
   .then(response => {res.json(response.data)})
   
   }
