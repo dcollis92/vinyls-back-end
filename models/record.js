ed5e1fc63f87490e7c8572b918db070e0ec056bc
dcollis92 <davidphilipcollis@gmail.com>
2022-03-30 13:22:15 -0500

    rating schema stub up

--- models/record.js
+++ models/record.js
@@ -2,6 +2,13 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const ratingSchema = new Schema({
+  rating: {type: Number, min: 1, max: 5, default: 5},
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+},{ 
+  timestamps: true
+})
+
 const recordSchema = new Schema({
   cover_image: String,
   //label: String,
@@ -11,11 +18,16 @@ const recordSchema = new Schema({
   thumb: String,
   title: String,
   uri: String,
-  year: Number
+  year: Number,
+  ratings: [ratingSchema]
 }, {
   timestamps: true
 })
 
 const Record = mongoose.model('Record', recordSchema)
+const Rating = mongoose.model("Rating", ratingSchema)
 
-export {Record}
+export {
+  Record,
+  Rating
+}
