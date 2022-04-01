4ace19e0eafad2382778ce98e1035c57517f3e82
provideforme <evan.a.bernard@gmail.com>
2022-04-01 01:10:05 -0400

    mvp

--- controllers/records.js
+++ controllers/records.js
@@ -25,10 +25,10 @@ function recordDetails(req, res) {
 }
 
 
-const addRating = async (req, res) {
-  console.log(req.body)
+const addRating = async (req, res) => {
+  console.log(req.body, "aaahahahahhahahahahahahahahahahahaha")
   try {
-   req.body.rating = req.user.profile
+  //  req.body.rating = req.user.profile
    const record = await Record.findById(req.params.id)
    record.ratings.push(req.body)
     await record.save()
